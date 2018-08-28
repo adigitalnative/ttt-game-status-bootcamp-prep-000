5a0c3167152c77dfa5794e09ce580c9c647ee25f
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-28 14:22:41 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,11 +14,19 @@ WIN_COMBINATIONS = [
   [2,4,6],
 ]
 
+# def won?(board)
+#   WIN_COMBINATIONS.detect do |combo|
+#     position_taken(board, combo[0]) &&
+#     board[combo[0]] == board[combo[1]] &&
+#     board[combo[1]] == board[combo[2]]
+#   end
+# end
+
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
-    position_taken(board, combo[0]) &&
     board[combo[0]] == board[combo[1]] &&
-    board[combo[1]] == board[combo[2]]
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
   end
 end
 
