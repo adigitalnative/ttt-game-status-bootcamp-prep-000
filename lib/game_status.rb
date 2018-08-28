0ffade947264f6ceb84de31d8f0086200ed28ceb
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-28 14:26:16 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -22,14 +22,6 @@ def won?(board)
   end
 end
 
-# def won?(board)
-#   WIN_COMBINATIONS.detect do |combo|
-#     board[combo[0]] == board[combo[1]] &&
-#     board[combo[1]] == board[combo[2]] &&
-#     position_taken?(board, combo[0])
-#   end
-# end
-
 def full?(board)
   if board.detect {|s| s == " "}
     return false
